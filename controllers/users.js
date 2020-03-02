e2ce9f19129c341a9f83a7cbf7592b0e57e4b6c3
Nathanael Fletcher <nathanaelfletcher19@gmail.com>
2020-03-02 17:26:31 -0500

    finalized user auth and implemented fail state messages so the entire site doesnt hang

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,17 @@ const users = express.Router()
 const User = require('../models/users.js')
 
 
+// Functions
+const isUnique = (name) => {
+    User.exists({username: name}, (err, result) => {
+        if (err) {
+            res.render('users/new-usernamefail.ejs')
+        } else {
+            console.log(result);
+            return result
+        }
+    })
+}
 // Show Sign Up Form
 users.get('/new', (req, res) => {
     res.render('users/new.ejs', {
@@ -13,17 +24,21 @@ users.get('/new', (req, res) => {
 
 // Create New User
 users.post('/', (req, res) => {
-    if (req.body.password1 === req.body.password2) {
-        req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
-        User.create(req.body, (err, createdUser) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('user is created', createdUser);
-                req.session.currentUser = createdUser
-                res.redirect('/')
-            }
-        })
+    if (req.body.password === req.body.password2) {
+        if (isUnique(req.body.username) === false) {
+            req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+            User.create(req.body, (err, createdUser) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('user is created', createdUser);
+                    req.session.currentUser = createdUser
+                    res.redirect('/')
+                }
+            })
+        } else {
+            res.render('users/new-usernamefail.ejs')
+        }
     } else {
         res.render('users/new-passfail.ejs')
     }
