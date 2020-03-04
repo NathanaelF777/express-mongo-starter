4b8e9e6fcc2fe0498b62909363fcec40d4545882
Nathanael Fletcher <nathanaelfletcher19@gmail.com>
2020-03-04 13:24:07 -0500

    finally fixed duplicate name checking

--- controllers/users.js
+++ controllers/users.js
@@ -4,18 +4,6 @@ const users = express.Router()
 const User = require('../models/users.js')
 
 
-// Functions
-const isUnique = (name) => {
-    User.exists({username: name}, (err, result) => {
-        if (err) {
-            res.render('users/new-usernamefail.ejs')
-            return result
-        } else {
-            console.log(result);
-            return result
-        }
-    })
-}
 // Show Sign Up Form
 users.get('/new', (req, res) => {
     res.render('users/new.ejs', {
@@ -24,13 +12,11 @@ users.get('/new', (req, res) => {
 })
 
 // Create New User
-users.post('/', (req, res) => {
+users.post('/', async (req, res) => {
     if (req.body.password === req.body.password2) {
-        if (async isUnique(req.body.username)) {
-            console.log(isUnique(req.body.username) + ' first');
+        if (await User.exists({username: req.body.username}).then()) {
             res.render('users/new-usernamefail.ejs')
             } else {
-                console.log(isUnique(req.body.username) + ' second');
             req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
             User.create(req.body, (err, createdUser) => {
                 if (err) {
